cd42a04f3b8c6d52d06d2577088123af758ca09b
Robin <574244038@qq.com>
2020-11-06 23:18:17 +0800

    update

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,17 @@
 const _import = require('../helper/utils/view-import/' + process.env.NODE_ENV)
 
 export default {
-    routes: [{
-            path: '/404',
-            component: _import('404')
-        },
-        {
-            path: '*',
-            component: _import('404')
-        }
-    ]
+	routes: [{
+		path: '/404',
+		component: _import('404')
+	},
+	{
+		path: '/',
+		component: _import('index')
+	},
+	{
+		path: '*',
+		component: _import('404')
+	}
+	]
 }
---
+++ src/views/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="body">
+		<div class="head">321</div>
+		<div class="content">123</div>
+	</div>
+</template>
+<style lang="less" scoped>
+.body{
+	font-size: 20px;
+}
+</style>
